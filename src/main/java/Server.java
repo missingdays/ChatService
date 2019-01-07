fa4df3c55a38c5a3bee1992008554d112b884837
Voronchikhin <m.voronchikhin@g.nsu.ru>
2019-01-08 03:10:07 +0700

    add logs

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,9 +8,11 @@ import java.io.IOException;
 public class Server {
     private static final Logger logger = LogManager.getLogger(Server.class);
     public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(System.getenv("PORT"));
         Undertow server = Undertow.builder()
-                .addHttpListener(Integer.parseInt(System.getenv("PORT")), "localhost", ChatHttpRootHandler.getInstance())
+                .addHttpListener(port, "0.0.0.0", ChatHttpRootHandler.getInstance())
                 .build();
+        logger.info("port: " + port);
         server.start();
     }
 

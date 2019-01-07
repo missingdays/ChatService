bf5ad564f88b68a6fc49e8e5045dcb6d59f65150
Voronchikhin <m.voronchikhin@g.nsu.ru>
2019-01-08 02:51:59 +0700

    change port

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,5 @@
 import handler.ChatHttpRootHandler;
 import io.undertow.Undertow;
-import io.undertow.util.Headers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +9,7 @@ public class Server {
     private static final Logger logger = LogManager.getLogger(Server.class);
     public static void main(String[] args) throws IOException {
         Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost", ChatHttpRootHandler.getInstance())
+                .addHttpListener(Integer.parseInt(System.getenv("PORT")), "localhost", ChatHttpRootHandler.getInstance())
                 .build();
         server.start();
     }

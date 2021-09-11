231ba77c420dbc867c158cb5056eb498a1f71da2
evgeny.bovykin <rebovykin@gmail.com>
2021-09-11 12:18:50 +0200

    Add bad code

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
+import java.util.*;
+
 public class Server {
     private static final Logger logger = LogManager.getLogger(Server.class);
     public static void main(String[] args) throws IOException {
